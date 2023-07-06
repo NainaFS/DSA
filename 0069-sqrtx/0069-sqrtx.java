3876a5e313104c65b234bfa85021662da9e4771e
NainaFS <136098921+NainaFS@users.noreply.github.com>
2023-07-06 19:58:43 +0530

    Time: 1 ms (100%), Space: 39.7 MB (70.03%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,18 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0) return 0;
-        long low = 1, high = x;
-        long ans = 0;
-        while(low<=high){
-            long mid = (low + high)/2;
-            if((mid*mid) <=x){
-                ans = mid;
-                low = mid+1;
-            } else {
-                high = mid -1;
-            }
-        }
-        return (int)ans;
-        
+       if(x==0) return 0;
+       long low=1;
+       long high = x;
+       long ans=0;
+       while(low<=high){
+           long mid= (low+high)/2;
+           if(mid*mid <= x){
+               ans=mid;
+               low=mid+1;
+           } else {
+               high = mid-1;
+           }
+       }
+       return (int)ans;
     }
 }
