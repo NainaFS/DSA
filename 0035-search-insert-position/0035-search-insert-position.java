9a18cd83637c27b00ca8e465fe729b0542ee6516
NainaFS <136098921+NainaFS@users.noreply.github.com>
2023-07-06 20:01:51 +0530

    Time: 0 ms (100%), Space: 42.9 MB (76.32%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,18 +1,18 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int low = 0;
+        int low=0;
         int high = nums.length-1;
-        int ans = 0;
+        int ans=0;
         while(low<=high){
             int mid = (low+high)/2;
-            if(nums[mid]<target){
-                ans = mid+1;
-                low = mid+1;
-            } else if(nums[mid]==target) {
-                ans = mid;
+            if(nums[mid]==target){
+                ans=mid;
                 break;
+            } else if(nums[mid]<target){
+                ans=mid+1;
+                low = mid+1;
             } else {
-                high = mid - 1;
+                high = mid-1;
             }
         }
         return ans;
