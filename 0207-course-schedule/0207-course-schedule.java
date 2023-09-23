fb816dc0af04ab3182c25c641841ce7fee5e8140
NainaFS <136098921+NainaFS@users.noreply.github.com>
2023-09-23 23:50:46 +0530

    Time: 6 ms (45.85%), Space: 44.6 MB (15.61%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0; i<numCourses; i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int i=0; i<prerequisites.length; i++){
+            int u=prerequisites[i][0];
+            int v = prerequisites[i][1];
+            graph.get(u).add(v);
+        }
+        int[] indegree = new int[numCourses];
+        for(int i=0; i<numCourses;i++){
+            for(int j: graph.get(i)){
+                indegree[j]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i]==0) q.add(i);
+        }
+        int count =0;
+        while(!q.isEmpty()){
+            int x=q.poll();
+            count++;
+            for(int j: graph.get(x)){
+                indegree[j]--;
+                if(indegree[j]==0) q.add(j);
+            }
+        }
+        return (count==numCourses);
+    }
+
+}
