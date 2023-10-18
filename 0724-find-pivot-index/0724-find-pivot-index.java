a6ae631c03d123268c4b506329c376dc81664232
NainaFS <136098921+NainaFS@users.noreply.github.com>
2023-10-18 14:00:40 +0530

    Time: 1 ms (99.01%), Space: 43.7 MB (83.91%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,15 +1,17 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int sum =0;
-        int lsum =0;
-        for(int i=0; i<nums.length; i++){
-            sum+=nums[i];
-        }
-        for(int i =0; i<nums.length; i++){
-            sum -= nums[i];
-            if(sum == lsum) return i;
-            lsum += nums[i];
-        }
-        return -1;
+       int lsum=0;
+       int rsum=0;
+       for(int i=0; i<nums.length; i++){
+           rsum+=nums[i];
+       }
+       for(int i=0; i<nums.length; i++){
+           rsum-=nums[i];
+           if(rsum==lsum){
+               return i;
+           }
+           lsum+=nums[i];
+       }
+       return -1;
     }
 }
